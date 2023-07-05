6b2ce63c74b4155ed7eb5d3ea01c57ca3ce84370
Denis Biryukov <denis.biriukov@geotoolkit.net>
2023-07-05 18:00:08 +0700

    Update ng to v11

--- src/app/components/multiwell/multiwell.component.spec.ts
+++ src/app/components/multiwell/multiwell.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { MultiWellComponent } from './multiwell.component';
 
@@ -6,7 +6,7 @@ describe('OverlayComponent', () => {
   let component: MultiWellComponent;
   let fixture: ComponentFixture<MultiWellComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ MultiWellComponent ]
     })
