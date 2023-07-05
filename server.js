cdf577285284ac6cb40b25fa3571ec71f3564e27
Denis Biryukov <denis.biriukov@geotoolkit.net>
2023-07-05 17:55:21 +0700

    Update ng to v11

--- server.js
+++ server.js
@@ -7,37 +7,37 @@ import cors from 'cors';
 import {loadRoutes} from './server/routes.js';
 
 // Code to run if we're in the master process
-if (cluster.isPrimary) {
-    // Count the machine's CPUs
-    const cpuCount = cpus.length;
+// if (cluster.isPrimary) {
+//     // Count the machine's CPUs
+//     const cpuCount = cpus.length;
+//
+//     // Create a worker for each CPU
+//     for (let i = 0; i < cpuCount; i += 1) {
+//         cluster.fork();
+//     }
+//
+//     // Listen for terminating workers
+//     cluster.on('exit', function(worker) {
+//     // Replace the terminated workers
+//         console.log('Worker ' + worker.id + ' died :(');
+//         cluster.fork();
+//     });
+//
+//     // Code to run if we're in a worker process
+// } else {
+const app = express();
 
-    // Create a worker for each CPU
-    for (let i = 0; i < cpuCount; i += 1) {
-        cluster.fork();
-    }
 
-    // Listen for terminating workers
-    cluster.on('exit', function(worker) {
-    // Replace the terminated workers
-        console.log('Worker ' + worker.id + ' died :(');
-        cluster.fork();
-    });
+app.use(cors());
+app.use(bodyParser.urlencoded({
+    extended: false,
+}));
+// parse application/json
+app.use(bodyParser.json());
 
-    // Code to run if we're in a worker process
-} else {
-    const app = express();
-
-
-    app.use(cors());
-    app.use(bodyParser.urlencoded({
-        extended: false,
-    }));
-    // parse application/json
-    app.use(bodyParser.json());
-
-    const port = process.env.PORT || 3000;
-    app.listen(port, function() {
-        loadRoutes(app);
-        console.log('Server running at http://127.0.0.1:' + port + '/');
-    });
-}
+const port = process.env.PORT || 3000;
+app.listen(port, function() {
+    loadRoutes(app);
+    console.log('Server running at http://127.0.0.1:' + port + '/');
+});
+// }
